9b262d9cd298802c82a62a66abeb07b7db0e3a14
Ian8829 <ghjkghj@naver.com>
2017-02-19 15:27:10 +0900

    add

---
+++ server.js
@@ -0,0 +1,10 @@
+/**
+ * Created by ian on 2017. 2. 19..
+ */
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => res.send('Hello David'));
+app.use(express.static(__dirname));
+app.listen(PORT, () => console.log('Server is running at ' + PORT));
