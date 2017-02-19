6e5c6f4ab7b358204afb3c344dcb0db371b1f8d1
Ian8829 <ghjkghj@naver.com>
2017-02-19 16:24:27 +0900

    revise

---
+++ middleWare.js
@@ -0,0 +1,16 @@
+/**
+ * Created by ian on 2017. 2. 19..
+ */
+
+const middleWare = {
+    requireAuthentication: (req, res, next) => {
+        console.log('Private route hit!');
+        next();
+    },
+    logger: (req, res, next) => {
+    console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
+    next();
+    }
+};
+
+module.exports = middleWare;
--- server.js
+++ server.js
@@ -4,7 +4,17 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const middleWare = require('./middleWare');
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => res.send('Hello David'));
-app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/img');
+
+// app.get('/', function(req, res){
+//     var path = req.path;
+//     res.locals.path = path;
+//     res.render('index');
+// });
+
 app.listen(PORT, () => console.log('Server is running at ' + PORT));
