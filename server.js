4771858f98a454116137b28eff300db426a9b966
Ian8829 <ghjkghj@naver.com>
2017-03-06 16:01:06 +0900

    pug

--- server.js
+++ server.js
@@ -3,18 +3,18 @@
  */
 const express = require('express');
 const app = express();
+const path = require('path');
 const PORT = process.env.PORT || 3000;
 const middleWare = require('./middleWare');
 
-app.use(express.static(__dirname + '/public'));
+const index = require('./routes/index');
 
-app.get('/', (req, res) => res.send('Hello David'));
-app.set('views', __dirname + '/img');
 
-// app.get('/', function(req, res){
-//     var path = req.path;
-//     res.locals.path = path;
-//     res.render('index');
-// });
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
 
 app.listen(PORT, () => console.log('Server is running at ' + PORT));
