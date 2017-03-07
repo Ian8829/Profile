bdbe65289a873b035561fe1a6e1d48987906c083
Ian8829 <ghjkghj@naver.com>
2017-03-07 19:55:19 +0900

    slider

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const path = require('path');
 const logger = require('morgan');
 const PORT = process.env.PORT || 3000;
-const expressVue = require('express-vue');
+// const expressVue = require('express-vue');
 // const middleWare = require('./middleWare');
 
 const index = require('./routes/index');
