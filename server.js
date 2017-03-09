8122db332d6f43f72d836dc19328261c386f96c8
Ian8829 <ghjkghj@naver.com>
2017-03-09 10:51:25 +0900

    revise

--- server.js
+++ server.js
@@ -6,17 +6,12 @@ const app = express();
 const path = require('path');
 const logger = require('morgan');
 const PORT = process.env.PORT || 3000;
-// const expressVue = require('express-vue');
-// const middleWare = require('./middleWare');
 
 const index = require('./routes/index');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 // main pug file
-
-// app.engine('vue', expressVue);
-// app.set('view engine', 'vue');
 
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -32,7 +27,16 @@ app.get('/profile', (req, res) => res.render('profile'));
 app.get('/sites', (req, res) => res.render('sites'));
 app.get('/music', (req, res) => res.render('music'));
 app.get('/login', (req, res) => res.render('login'));
+// sub pages
 
+app.use((req, res, next) => res.status(404).send('404 - Not Found'));
+// 404 page
+
+app.use((err, req, res, next) => {
+   console.error(err.stack);
+   res.status(500).send('500 - Server Error');
+});
+// 500 page
 
 app.listen(PORT, () => console.log('Server is running at ' + PORT));
 // listen port
